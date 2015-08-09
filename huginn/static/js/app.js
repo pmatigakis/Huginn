c2813d3f01b46cf1246a52a1bc84f91ff8802aac
Panagiotis Matigakis <pmatigakis@gmail.com>
2015-08-09 18:12:43 +0300

    Removed openlayers and now use leaflet instead.

--- huginn/static/js/app.js
+++ huginn/static/js/app.js
@@ -1,25 +1,45 @@
-var view = new ol.View({
-	center: ol.proj.transform([37.41, 8.82], 'EPSG:4326', 'EPSG:3857'),
-	zoom: 4
-});
-  
-var map = new ol.Map({
-	target: 'map',
-	layers: [
-	         new ol.layer.Tile({
-	        	 source: new ol.source.MapQuest({layer: 'osm'})
-	         })
-	         ],
-	view: view
-});
-      
-setInterval(function(){
-	$.getJSON("fdmdata", function(data){
-	$("#location .longitude").text(data.fdm_data["position/long-gc-deg"]);
-	$("#location .latitude").text(data.fdm_data["position/lat-gc-deg"]);
-	$("#location .altitude").text(data.fdm_data["position/h-sl-ft"]);
-    $("#location .airspeed").text(data.fdm_data["velocities/vtrue-kts"]);
-    $("#location .heading").text(data.fdm_data["attitude/heading-true-rad"] / 0.0174532925);
-      
-    view.setCenter(ol.proj.transform([data.fdm_data["position/long-gc-deg"], data.fdm_data["position/lat-gc-deg"]], 'EPSG:4326', 'EPSG:3857'));});
-}, 1000);
+var map;
+var follow_aircraft;
+var aircraft_marker;
+
+function init_map(){
+	map = L.map('map').setView([51.0, 0.0], 13);
+	
+	L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+		attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+	}).addTo(map);
+	
+	aircraft_marker = L.marker([51.0, 0.0]).addTo(map);
+} 
+
+function start_map_update(){
+	setInterval(function(){
+		$.getJSON("fdmdata", function(data){
+			$("#location .longitude").text(data.fdm_data["position/long-gc-deg"]);
+			$("#location .latitude").text(data.fdm_data["position/lat-gc-deg"]);
+			$("#location .altitude").text(data.fdm_data["position/h-sl-ft"]);
+			$("#location .airspeed").text(data.fdm_data["velocities/vtrue-kts"]);
+			$("#location .heading").text(data.fdm_data["attitude/heading-true-rad"] / 0.0174532925);
+	      
+			var latitude = data.fdm_data["position/lat-gc-deg"];
+			var longitude = data.fdm_data["position/long-gc-deg"];
+			
+			if(follow_aircraft){
+	    		map.setView([latitude, longitude], 13);
+			}
+			
+			aircraft_marker.setLatLng([latitude, longitude]);
+		});
+	}, 1000);
+}
+
+$(document).ready(function(){
+	init_map();
+	start_map_update();
+	
+	follow_aircraft = $("#follow_aircraft").is(":checked");
+	
+	$("#follow_aircraft").click(function(){
+		follow_aircraft = this.checked;
+	});
+})
