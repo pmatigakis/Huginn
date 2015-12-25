ad2c3ca50062d0865a747f97cb4ec291af2417e7
Panagiotis Matigakis <pmatigakis@gmail.com>
2015-12-25 12:51:20 +0200

    Improved the primary flight data display on the web front end

--- huginn/static/js/avionics.js
+++ huginn/static/js/avionics.js
@@ -13,8 +13,13 @@ Avionics.PrimaryFlightDisplay = function(canvas){
 	this.pitch = 0.0;
 	
 	this.altitude = 0.0;
+	this.altitudeUnits = "meters";
+	
 	this.airspeed = 0.0;
+	this.airspeedUnits = "m/s";
+	
 	this.heading = 0.0;
+	this.headingUnits = "degrees";
 	
 	this.displayWidth = canvas.width;
 	this.displayHeight = canvas.height;
@@ -32,37 +37,82 @@ Avionics.PrimaryFlightDisplay.prototype.drawFlightData = function(){
 	var context = this.context;
 	
 	context.font = this.textSize + "px Sans-Serif";
-	context.fillStyle = this.flightDataTextColor;
+
+	var headingText = Math.round(this.heading).toString();
 	
+	var textWidth = context.measureText(headingText).width;
+	var unitTextWidth = context.measureText(this.headingUnits).width; 
+	if(unitTextWidth > textWidth){
+		textWidth = unitTextWidth; 
+	}
+
+	context.fillStyle = "#000000";
+	context.fillRect((this.displayWidth/2.0) - (textWidth/2.0)*1.2 , 0, textWidth*1.2, this.textSize*2.2);
+	
+	context.fillStyle = this.flightDataTextColor;
 	context.textBaseline = "top";
 	context.textAlign = "center";
-	context.fillText(Math.round(this.heading), this.displayWidth/2.0, 0);
+	context.fillText(headingText, this.displayWidth/2.0, 0);
+	context.fillText(this.headingUnits, this.displayWidth/2.0, this.textSize);
 	
+	var altitudeText = Math.round(this.altitude).toString(); 
+	
+	var textWidth = context.measureText(altitudeText).width;
+	var unitTextWidth = context.measureText(this.altitudeUnits).width; 
+	if(unitTextWidth > textWidth){
+		textWidth = unitTextWidth; 
+	}
+
+	context.fillStyle = "#000000";
+	context.fillRect(this.displayWidth - (textWidth*1.2), (this.displayHeight/2.0) - this.textSize, textWidth*1.2, (this.textSize*2.0)*1.4);
+	
+	context.fillStyle = this.flightDataTextColor;
 	context.textBaseline = "middle";
 	context.textAlign = "right";
-	context.fillText(Math.round(this.altitude), this.displayWidth, this.displayHeight/2.0);
+	context.fillText(altitudeText, this.displayWidth, this.displayHeight/2.0);
+	context.fillText(this.altitudeUnits, this.displayWidth, (this.displayHeight/2.0) + this.textSize);
+	
+	var airspeedText = Math.round(this.airspeed).toString();
+	
+	var textWidth = context.measureText(airspeedText).width;
+	var unitTextWidth = context.measureText(this.airspeedUnits).width; 
+	if(unitTextWidth > textWidth){
+		textWidth = unitTextWidth; 
+	}
+
+	context.fillStyle = "#000000";
+	context.fillRect(0.0, (this.displayHeight/2.0) - this.textSize, textWidth*1.2, (this.textSize*2.0)*1.4);
+	
+	context.fillStyle = this.flightDataTextColor;
 	
 	context.textAlign = "left";
-	context.fillText(Math.round(this.airspeed), 0, this.displayHeight/2.0);
+	context.fillText(airspeedText, 0, this.displayHeight/2.0);
+	context.fillText(this.airspeedUnits, 0, (this.displayHeight/2.0) + this.textSize);
 }
 
 Avionics.PrimaryFlightDisplay.prototype.drawHorizon = function(){
 	var context = this.context;
 	
-	//draw the sky. Basically just fill everything with blue
 	context.fillStyle = this.skyColor;
 	context.fillRect(0, 0, this.displayWidth, this.displayHeight);
 
-	var p = (this.pitch * (9 * this.pitchSpacing))/ 90.0;
-	//p *= 2.0;
+	var draw_pitch = this.pitch;
+	if (draw_pitch > 90.0){
+		draw_pitch = 90.0;
+	}else if(draw_pitch < -90.0){
+		draw_pitch = -90.0;
+	}
+	
+	var p = (draw_pitch * (9 * this.pitchSpacing))/ 90.0;
 	
 	context.save();
-	//context.translate(this.displayWidth/2.0, this.displayHeight/2.0);
 	context.translate(this.displayWidth/2.0, this.displayHeight/2.0);
-	context.rotate(-this.roll * (Math.PI / 180));
+	
+	var draw_roll = this.roll % 360.0;
+
+	context.rotate(-draw_roll * (Math.PI / 180));
 	context.translate(0, p);
 	
-	//draw the ground
 	context.fillStyle = this.groundColor;
 	context.fillRect(-this.displayWidth, 0, this.displayWidth*2.0, this.displayHeight*2.0);
 	
@@ -76,13 +126,21 @@ Avionics.PrimaryFlightDisplay.prototype.drawPitchAngles = function(){
 	context.textBaseline = "middle";
 	context.fillStyle = this.pitchAngleTextColor;
 	
-	var p = (this.pitch * (9 * this.pitchSpacing))/ 90.0;
-	//p *= 2.0;
+	var draw_pitch = this.pitch;
+	if (draw_pitch > 90.0){
+		draw_pitch = 90.0;
+	}else if(draw_pitch < -90.0){
+		draw_pitch = -90.0;
+	}
+	
+	var p = (draw_pitch * (9 * this.pitchSpacing))/ 90.0;
 	
 	context.save();
-	//context.translate(this.displayWidth/2.0, this.displayHeight/2.0);
 	context.translate(this.displayWidth/2.0, this.displayHeight/2.0);
-	context.rotate(-this.roll * (Math.PI / 180));
+	
+	var draw_roll = this.roll % 360.0;
+
+	context.rotate(-draw_roll * (Math.PI / 180));
 	context.translate(0, p);
 	
 	context.strokeStyle = "white";
@@ -120,11 +178,11 @@ Avionics.PrimaryFlightDisplay.prototype.drawPitchAngles = function(){
 Avionics.PrimaryFlightDisplay.prototype.drawAircraft = function() {
 	var context = this.context;
 	
-	context.strokeStyle = "black";
+	context.strokeStyle = "yellow";
 	context.lineWidth = this.aircraftLineWidth;
 	context.lineCap = 'square';
 	
-	var p1x = 0.2 * this.displayWidth;
+	var p1x = 0.3 * this.displayWidth;
 	var p2x = 0.4 * this.displayWidth;
 	var p3y = (this.displayHeight/2.0) + this.displayHeight * 0.05;
 	
@@ -135,7 +193,7 @@ Avionics.PrimaryFlightDisplay.prototype.drawAircraft = function() {
 	context.stroke();
 	context.closePath();
 	
-	var p1x = 0.8 * this.displayWidth;
+	var p1x = 0.7 * this.displayWidth;
 	var p2x = 0.6 * this.displayWidth;
 	var p3y = (this.displayHeight/2.0) + this.displayHeight * 0.05;
 	
